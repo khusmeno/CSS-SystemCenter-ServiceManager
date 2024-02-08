610c84952a9917a2e6dd554feea89367a23f45ba
name in public profle <khusmeno@hotmail.com>
2024-02-08 10:32:23 +0300

    Delete MicrosoftSupport.SCSM directory

--- MicrosoftSupport.SCSM/Build/Seal_Import_HealthStatus.Core.ps1
+++
@@ -1,9 +0,0 @@
-if (-not (Get-Module -name System.Center.Service.Manager)) { Import-Module ((Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\System Center\2010\Service Manager\Setup').InstallDirectory +'PowerShell\' +'System.Center.Service.Manager.psd1') -Force }
-cd C:\GitHub\khusmeno\CSS-SystemCenter-ServiceManager\MicrosoftSupport.SCSM
-
-C:\Users\khusmeno\Desktop\kh\FastSeal.exe MicrosoftSupport.SCSM.HealthStatus.Core.xml /keyfile C:\Users\khusmeno\Desktop\kh\kh.snk /company kh
-
-Import-SCSMManagementPack -Fullname MicrosoftSupport.SCSM.HealthStatus.Core.mp
-
-read-host " "
-
